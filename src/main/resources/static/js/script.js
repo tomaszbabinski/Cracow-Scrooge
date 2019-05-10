e24d774d86de468821415f873a0f14a52f702fc8
tomaszbabinski <tommybab@gmail.com>
2019-05-10 23:04:53 +0200

    Added navbar to all views and changing css

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -9,7 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
     renderBestOffersPerProduct();
     setInterval(renderBestOffersPerProduct, 1000 * 15);
 
+    var divWithchart = document.getElementById("chartDiv");
+    divWithchart.addEventListener("onresize",resize)
+
 });
+
+function resize() {
+    console.log("hi");
+}
 
 function renderChart() {
 
@@ -68,7 +75,8 @@ function renderChart() {
                             fontColor: 'white',
                             fontSize: 20
                         }
-                    }
+                    },
+                    responsive: true
                 }
 
 
@@ -77,6 +85,8 @@ function renderChart() {
             savings = values.reduce(getSum)
             savings = Math.round(savings*100)/100;
             totalSavings.innerText = "Hey Scrooge you managed to save " + savings.toString() + " money!!!";
+
+
         }
     })
 }
@@ -101,21 +111,25 @@ function renderBestOffersPerProduct() {
 
             var dataMap = new Map();
             var shopMap = new Map();
+            var efficiencyMap = new Map();
             for (const offer of response) {
 
                 dataMap.set(offer.product.brand + " " + offer.product.name, offer.price);
                 shopMap.set(offer.product.brand + " " + offer.product.name, offer.shopName);
+                efficiencyMap.set(offer.product.brand + " " + offer.product.name, offer.product.averageEfficiency);
             }
             let shopNames = Array.from(shopMap.values());
+            let efficiencies = Array.from(efficiencyMap.values());
             let keys = Array.from(dataMap.keys());
             let values = Array.from(dataMap.values());
-            var table = " <table class=\"table table-striped\">\n" +
+            var table = " <table class=\"table table-striped mt-sm-4\">\n" +
                 "<caption style='text-align: center'>Your best offers</caption>\n" +
                 "        <thead class=\"thead-light\">\n" +
                 "\n" +
                 "        <th scope=\"col\">Name</th>\n" +
                 "        <th scope=\"col\">Price</th>\n" +
                 "        <th scope=\"col\">Shop</th>\n" +
+                "        <th scope=\"col\">Efficiency</th>\n" +
                 "        </thead>\n" +
                 "        <tbody>";
             var randomDataToshow = new Set();
@@ -126,9 +140,9 @@ function renderBestOffersPerProduct() {
 
             var arrayFromData = Array.from(randomDataToshow);
 
-            table += "<tr><td>" + keys[arrayFromData[0]] + "</td><td>" + values[arrayFromData[0]] + "</td><td>" + shopNames[arrayFromData[0]] + "</td></tr>";
-            table += "<tr><td>" + keys[arrayFromData[1]] + "</td><td>" + values[arrayFromData[1]] + "</td><td>" + shopNames[arrayFromData[1]] + "</td></tr>";
-            table += "<tr><td>" + keys[arrayFromData[2]] + "</td><td>" + values[arrayFromData[2]] + "</td><td>" + shopNames[arrayFromData[2]] + "</td></tr>";
+            table += "<tr><td>" + keys[arrayFromData[0]] + "</td><td>" + values[arrayFromData[0]] + "</td><td>" + shopNames[arrayFromData[0]] + "</td><td>"+ efficiencies[arrayFromData[0]] + "</td></tr>";
+            table += "<tr><td>" + keys[arrayFromData[1]] + "</td><td>" + values[arrayFromData[1]] + "</td><td>" + shopNames[arrayFromData[1]] + "</td><td>"+ efficiencies[arrayFromData[1]] + "</td></tr>";
+            table += "<tr><td>" + keys[arrayFromData[2]] + "</td><td>" + values[arrayFromData[2]] + "</td><td>" + shopNames[arrayFromData[2]] + "</td><td>"+ efficiencies[arrayFromData[2]] + "</td></tr>";
             table += "</tbody></table>";
             bestOfferDiv.innerHTML = table;
 
